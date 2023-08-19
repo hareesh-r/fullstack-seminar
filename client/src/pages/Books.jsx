b6a87d359967ce94fa171a4a1bdf8865449ea74d
hareesh-r <hareeshprogrammer@gmail.com>
2023-08-19 18:49:52 +0530

    feat: initial commit

---
+++ client/src/pages/Books.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books");
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  console.log(books);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8800/books/${id}`);
+      window.location.reload()
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Hareesh Book Shop</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div key={book.id} className="book">
+            <img src={book.cover} alt="" />
+            <div className="book-bottom">
+              <h2>{book.title}</h2>
+              <p>{book.desc}</p>
+              <span className="book-price">${book.price}</span>
+              <div className="button-grp">
+                <button className="delete" onClick={() => handleDelete(book.id)}>Delete</button>
+                <button className="update">
+                  <Link
+                    to={`/update/${book.id}`}
+                    style={{ color: "inherit", textDecoration: "none" }}
+                  >
+                    Update
+                  </Link>
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <button className="addHome">
+        <Link to="/add" style={{ color: "inherit", textDecoration: "none" }}>
+          Add new book
+        </Link>
+      </button>
+    </div>
+  );
+};
+
+export default Books;
